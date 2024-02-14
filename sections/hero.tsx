7be1c03b1ee68c1d68b90933de17fbf2284f7c4a
Lindrit <lindritsulaj2.0@gmail.com>
2024-02-14 22:04:15 +0100

    added hero image

--- sections/hero.tsx
+++ sections/hero.tsx
@@ -6,9 +6,9 @@ import { Button } from '@/components/ui/button'
 
 export default function Hero() {
   return (
-    <section className='pt-20 pb-28 md:pt-28 md:pb-36 bg-palette-isabelline px-4 md:px-6'>
-      <div className="max-w-screen-xl mx-auto">
-        <div className="w-full lg:w-3/5">
+    <section className='pb-16 bg-palette-isabelline px-4 md:px-6'>
+      <div className="max-w-screen-xl mx-auto flex flex-col lg:flex-row lg:items-end xl:items-center">
+        <div className="w-full lg:w-3/5 pb-10 pt-20 md:pt-28 md:pb-28">
           <h3 className='font-medium tracking-tight text-neutral-700 flex items-center gap-2 md:text-lg lg:text-xl uppercase'>
             <svg width="100%" height="100%" viewBox="0 0 24 24" className='w-6 md:w-8 text-palette-orange' fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 2V22M19.0711 4.92893L4.92893 19.0711M22 12H2M19.0711 19.0711L4.92893 4.92893" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
@@ -47,7 +47,9 @@ export default function Hero() {
             </ul>
           </div>
         </div>
-
+        <div className="w-full flex flex-col items-center lg:w-2/5">
+          <img className='max-h-[400px] lg:max-h-none lg:h-[95%] w-auto mt-auto' src="https://ik.imagekit.io/0s9lwb2yr/Lindrit%20Sulaj/lindrit%20sulaj%20hero%20image_09qJEnj4f.png?updatedAt=1707943804066" alt="Lindrit Sulaj" />
+        </div>
       </div>
 
     </section >
